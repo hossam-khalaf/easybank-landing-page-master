5e59e56bd18148fabc370384b54b3065b3a5f306
hossam-khalaf <hossamhassan303089@gmail.com>
2021-07-14 13:29:31 +0200

    animatied burger menu and overlay

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,15 @@
-console.log('HEyyyyyyyyyy')
+const btnHamburger = document.querySelector('.header__menu')
+const header = document.querySelector('.header')
+const overlay = document.querySelector('.overlay')
+
+btnHamburger.addEventListener('click', () => {
+	header.classList.toggle('open')
+
+	if (header.classList.contains('open')) {
+		overlay.classList.remove('fade-out')
+		overlay.classList.add('fade-in')
+	} else {
+		overlay.classList.remove('fade-in')
+		overlay.classList.add('fade-out')
+	}
+})
