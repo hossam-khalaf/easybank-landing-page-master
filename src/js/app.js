62277ab22652eb19c1c1a360f46992be28518e6f
hossam-khalaf <hossamhassan303089@gmail.com>
2022-08-29 12:41:51 +0700

    animated overlay

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,11 @@
-console.log('first')
+const header = document.querySelector('header')
+
+const hamburgerBtn = document.querySelector('#hamburgerBtn')
+
+const overlay = document.querySelector('header > div')
+
+hamburgerBtn.addEventListener('click', () => {
+	header.classList.toggle('opened')
+	overlay.classList.toggle('overlay')
+	overlay.classList.toggle('fade-in')
+})
