b040041ced9e1fd5178c60d8593476a36957944c
hossam-khalaf <hossamhassan303089@gmail.com>
2022-08-29 15:00:31 +0700

    hide && show onClick

--- src/js/app.js
+++ src/js/app.js
@@ -4,8 +4,12 @@ const hamburgerBtn = document.querySelector('#hamburgerBtn')
 
 const overlay = document.querySelector('header > div')
 
+const mobileMenu = document.querySelector('.header__mobile-menu')
+
 hamburgerBtn.addEventListener('click', () => {
 	header.classList.toggle('opened')
 	overlay.classList.toggle('overlay')
 	overlay.classList.toggle('fade-in')
+	mobileMenu.classList.toggle('show')
+	mobileMenu.classList.toggle('fade-in')
 })
